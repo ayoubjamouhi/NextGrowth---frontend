1c5f446b6f3d27f4c6956a10d995f786f95c041f
Ayoub JAMOUHI <gemouhi@gmail.com>
2021-07-26 01:20:59 +0100

    first commit

---
+++ src/components/Table.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Row from './Row';
+const TableStyle = styled.table`
+  text-align: center;
+`;
+function Table() {
+  const [users, setUsers] = useState([
+    {
+      id: "123456789",
+      createdDate: "2021-01-06T00:00:00.000Z",
+      status: "En validation",
+      firstName: "Mohamed",
+      lastName: "Taha",
+      userName: "mtaha",
+      registrationNumber: "2584",
+    },
+     {
+      id: "987654321",
+      createdDate: "2021-07-25T00:00:00.000Z",
+      status: "Validé",
+      firstName: "Hamid",
+      lastName: "Orrich",
+      userName: "horrich",
+      registrationNumber: "1594",
+    },
+       {
+      id: "852963741",
+      createdDate: "2021-09-15T00:00:00.000Z",
+      status: "Rejeté",
+      firstName: "Rachid",
+      lastName: "Mahidi",
+      userName: "rmahidi",
+      registrationNumber: "3576",
+    }
+  ]);
+  return (
+  <TableStyle>
+    <tr>
+      <th>ID</th>
+      <th>Date de création</th>
+      <th>Etat</th>
+      <th>Nom</th>
+      <th>Prénom</th>
+      <th>Nom d'utilisateur</th>
+      <th>Matricule</th>
+      <th>Action</th>
+    </tr>
+    {users.map(user => <Row user={user} />)}
+  </TableStyle>
+  )
+};
+
+export default Table;
